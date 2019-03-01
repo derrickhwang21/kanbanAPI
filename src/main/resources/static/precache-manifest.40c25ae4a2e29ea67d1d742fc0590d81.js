dcf5c6c90c16f91aec4a317ea8a09c78e6fd6044
dhwang21 <hwang.derrick@gmail.com>
2019-02-28 21:10:26 -0800

    added in react build

---
+++ src/main/resources/static/precache-manifest.40c25ae4a2e29ea67d1d742fc0590d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3128c2af23ee849e2e20",
+    "url": "/static/js/main.3128c2af.chunk.js"
+  },
+  {
+    "revision": "8fd48e1f719155b5bbe5",
+    "url": "/static/js/2.8fd48e1f.chunk.js"
+  },
+  {
+    "revision": "3128c2af23ee849e2e20",
+    "url": "/static/css/main.fbcc3513.chunk.css"
+  },
+  {
+    "revision": "8fd48e1f719155b5bbe5",
+    "url": "/static/css/2.e82564fd.chunk.css"
+  },
+  {
+    "revision": "46dbe50665ad215ff8c13adfada93043",
+    "url": "/index.html"
+  }
+];
